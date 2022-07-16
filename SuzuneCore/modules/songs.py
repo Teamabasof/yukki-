9af96b2cf46e5144940731db1b485c4d329c7cbc
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-07-16 23:01:09 +0530

    Update songs.py

--- SuzuneCore/modules/songs.py
+++ SuzuneCore/modules/songs.py
@@ -54,7 +54,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"🎙 **sᴏɴɢ**: [{title[:35]}]({link})\n🎬 **sᴏᴜʀᴄᴇ**: YouTube\n⏱️ **ᴅᴜʀᴀᴛɪᴏɴ**: `{duration}`\n👁‍🗨 **ᴠɪᴇᴡs**: `{views}`\n📤 **ᴜᴘʟᴏᴀᴅᴇʀ**: @{BOT_USERNAME}"
+        rep = f"Song : [{title[:35]}]({link})\n Source : YouTube\n⏱️ Duration : `{duration}`\n👁‍🗨 Views : `{views}`\n📤 Uploader : @{BOT_USERNAME}"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
